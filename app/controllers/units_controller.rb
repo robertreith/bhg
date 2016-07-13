ac90a0b2f2572e1791ce6e9c7e5c1cf1fe3c7cf6
RobertReith <robertmreith@gmail.com>
2016-07-13 00:45:49 -0400

    Update app to client specs

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -18,6 +18,27 @@ class UnitsController < ApplicationController
     end
   end
 
+  def edit
+    @unit = Unit.find_by(id: params[:id])
+  end
+
+  def update
+    @unit = Unit.find_by(id: params[:id])
+
+    if @unit.update(unit_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    unit = Unit.find_by(id: params[:id])
+    unit.destroy
+
+    redirect_to root_path
+  end
+
   private
     def unit_params
       params.require(:unit).permit(:num_of_units, :property_id, :percentage_id)
